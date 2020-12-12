86c671d00e3ca15837f7f9554ffc58bcdc01f034
Rachel Xiao <rachelxiao907@gmail.com>
2020-12-12 18:00:44 -0500

    Created private helper and add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -4,13 +4,32 @@ import java.io.*;
 //from least to greatest. It is important to realize that this requires
 //that the type is Comparable it would also require that there are no
 //null values, which is why it will extend the NoNullArrayList.
-OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   public OrderedArrayList() {
     super();
   }
 
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
+  }
+
+  public boolean add(T element) {
+    int old = super.size();
+    super.add(index(element), element);
+    return super.size() == old + 1;
+  }
+
+  private int index(T element) {
+    int index = super.size() - 1;
+    if (super.size() == 0) {
+      index = 0;
+    }
+    for (int i = 0; i < super.size()-1; i++) {
+      if (element.compareTo(super.get(i)) <= 0 && element.compareTo(super.get(i+1)) >= 0) {
+        index = i;
+      }
+    }
+    return index;
   }
 
 }
