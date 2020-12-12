63b05ebe641ffd422c776cbc3e9e033dcc10f1f0
Rachel Xiao <rachelxiao907@gmail.com>
2020-12-12 17:39:43 -0500

    Created constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+//An OrderedArrayList is an ArrayList that maintains all elements in order
+//from least to greatest. It is important to realize that this requires
+//that the type is Comparable it would also require that there are no
+//null values, which is why it will extend the NoNullArrayList.
+OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList() {
+    super();
+  }
+
+  public OrderedArrayList(startingCapacity) {
+    super(startingCapacity);
+  }
+
+}
